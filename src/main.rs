8a27a71b9d573c6491c745953830008b756008fd
Misza <misza@misza.net>
2022-03-09 20:48:24 +0100

    initial parser

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
+use std::fs;
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
 use pest::Parser;
+use pest::iterators::Pair;
 
 #[derive(Parser)]
-#[grammar = "ident.pest"]
-struct IdentParser;
+#[grammar = "assembly_94.pest"]
+struct AssemblyParser;
 
 fn main() {
-    let pairs = IdentParser::parse(Rule::ident_list, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
+    let warrior = fs::read_to_string("warriors/imp.red").expect("Could not read input file");
 
-    // Because ident_list is silent, the iterator will contain idents
-    for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
+    println!("{}", warrior);
+    
+    let mut warrior = AssemblyParser::parse(Rule::assembly_file, &warrior).unwrap_or_else(|e| panic!("{}", e));
 
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
-                _ => unreachable!()
-            };
-        }
+    print_rules(warrior.next().unwrap(), 0);
+}
+
+fn print_rules(pair: Pair<Rule>, level: u32) {
+    for _ in 0..level {
+        print!(" ");
+    }
+
+    print!("{:?}", pair.as_rule());
+
+    match pair.as_rule() {
+        Rule::comment => println!(": {}", pair.as_str()),
+        Rule::instruction => println!(": {}", pair.as_str()),
+        _ => println!()
+    }
+
+    for inner in pair.into_inner() {
+        print_rules(inner, level + 1);
     }
 }
---
+++ warriors/imp.red
@@ -0,0 +1,6 @@
+;redcode
+
+;name          Dwarf
+;author        A. K. Dewdney
+
+MOV 0, 1       ;copy current instruction 1 cell ahead
